ff2517528739f63024e5eac5c4b023e75ff720bb
roomartiv <159615365+roomartiv@users.noreply.github.com>
2024-04-05 18:02:08 +0000

    task2.10

--- task2.10.py
+++ task2.10.py
@@ -1,10 +1,7 @@
 class Song:
-    def __init__(self, title, artist, release_year):
+    def __init__(self, title, artist, release_year) -> None:
         self.title = title
         self.artist = artist
         self.release_year = release_year
-    def introduce(self):
-        print(self.title, self.artist, self.release_year)    
-some_song = Song('Какая-то песня', 'Кто-то', 2024)
-some_song.introduce()
-        
+some_song= Song('Какая-то песня', 'Кто-то', 2024)
+print(some_song)
